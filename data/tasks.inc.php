5c7eb4368a11be4455f01aada44283cbfc9d462f
farcall <linyizxw@gmail.com>
2015-11-19 10:23:19 +0800

    store模板去掉top.html中的"优惠券"导航

--- data/tasks.inc.php
+++ data/tasks.inc.php
@@ -5,8 +5,8 @@ return array (
   array (
     'cycle' => 'custom',
     'interval' => 3600,
-    'due_time' => 1447898787,
-    'last_time' => 1447895187,
+    'due_time' => 1447902388,
+    'last_time' => 1447898788,
   ),
 );
 
