d00b69656f30d6b12bd8eb0e0b46f306941dcdb4
farcall <linyizxw@gmail.com>
2015-11-24 16:23:44 +0800

    满100送60完成

--- data/tasks.inc.php
+++ data/tasks.inc.php
@@ -5,8 +5,8 @@ return array (
   array (
     'cycle' => 'custom',
     'interval' => 3600,
-    'due_time' => 1448105444,
-    'last_time' => 1448101844,
+    'due_time' => 1448353878,
+    'last_time' => 1448350278,
   ),
 );
 
