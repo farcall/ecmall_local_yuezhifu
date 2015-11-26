24c708c2bd56cfff16ee41d614abc6e4c9e175dc
farcall <linyizxw@gmail.com>
2015-11-27 05:56:18 +0800

    增加ecm_fanli_setting表对应的fanli_setting模块文件

---
+++ includes/models/fanli_setting.model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaokang
+ * Date: 2015/11/27
+ * Time: 5:54
+ */
+class Fanli_settingModel extends BaseModel
+{
+    var $table  = 'fanli_setting';
+    var $prikey = 'id';
+    var $_name  = 'fanli_setting';
+}
+?>
