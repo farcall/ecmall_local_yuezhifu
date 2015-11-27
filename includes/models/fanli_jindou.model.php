05037a3b7a5ef2977b3fa287fb8771801f28aecf
farcall <linyizxw@gmail.com>
2015-11-28 02:23:57 +0800

    fanli_jindou.model.php

---
+++ includes/models/fanli_jindou.model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaokang
+ * Date: 2015/11/28
+ * Time: 2:23
+ */
+class Fanli_jindouModel extends BaseModel
+{
+    var $table  = 'fanli_jindou';
+    var $prikey = 'id';
+    var $_name  = 'fanli_jindou';
+}
+?>
