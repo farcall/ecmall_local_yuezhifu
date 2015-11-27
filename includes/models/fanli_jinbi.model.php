408e58e191aebbf0db1d1c4d56dc3a11c3cc52af
farcall <linyizxw@gmail.com>
2015-11-27 22:33:07 +0800

    增加fanli_jinbi.model.php

---
+++ includes/models/fanli_jinbi.model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaokang
+ * Date: 2015/11/27
+ * Time: 22:32
+ */
+class Fanli_jinbiModel extends BaseModel
+{
+    var $table  = 'fanli_jinbi';
+    var $prikey = 'id';
+    var $_name  = 'fanli_jinbi';
+}
+?>
