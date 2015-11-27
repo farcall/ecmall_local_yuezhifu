746f0be32d46208e8de11a127356722a42f4aaa8
farcall <linyizxw@gmail.com>
2015-11-27 17:40:52 +0800

    增加fanli_operate.model.php文件

---
+++ includes/models/fanli_operate.model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaokang
+ * Date: 2015/11/27
+ * Time: 5:54
+ */
+class Fanli_operateModel extends BaseModel
+{
+    var $table  = 'fanli_operate';
+    var $prikey = 'id';
+    var $_name  = 'fanli_operate';
+}
+?>
