06011ab9ec32cd4cc07eadd1cb94293a19450d0a
farcall <linyizxw@gmail.com>
2015-12-06 22:50:19 +0800

    修复/index.php?app=search&cate_id=xx右侧分类错位问题

--- data/tasks.inc.php
+++ data/tasks.inc.php
@@ -5,8 +5,8 @@ return array (
   array (
     'cycle' => 'custom',
     'interval' => 3600,
-    'due_time' => 1449412553,
-    'last_time' => 1449408953,
+    'due_time' => 1449416153,
+    'last_time' => 1449412553,
   ),
 );
 
