c1aad885a6001de3e2b5ac6647fd76662fadfa5c
farcall <linyizxw@gmail.com>
2015-12-07 00:26:19 +0800

    修复头像上传时无法预览本地图片

--- data/tasks.inc.php
+++ data/tasks.inc.php
@@ -5,8 +5,8 @@ return array (
   array (
     'cycle' => 'custom',
     'interval' => 3600,
-    'due_time' => 1449416153,
-    'last_time' => 1449412553,
+    'due_time' => 1449419787,
+    'last_time' => 1449416187,
   ),
 );
 
