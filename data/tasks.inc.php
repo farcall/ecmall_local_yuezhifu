9777e3d33241b3e2b9f502675c855f708e088fb6
farcall <linyizxw@gmail.com>
2015-12-15 18:54:36 +0800

    加大对开店提交表单的验证

--- data/tasks.inc.php
+++ data/tasks.inc.php
@@ -5,8 +5,8 @@ return array (
   array (
     'cycle' => 'custom',
     'interval' => 3600,
-    'due_time' => 1450176070,
-    'last_time' => 1450172470,
+    'due_time' => 1450179673,
+    'last_time' => 1450176073,
   ),
 );
 
