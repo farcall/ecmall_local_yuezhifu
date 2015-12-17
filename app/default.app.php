c72d28682aa9b74a405c3270742c97782116be82
farcall <linyizxw@gmail.com>
2015-12-17 20:11:02 +0800

    首页SEO去掉多余选项

--- app/default.app.php
+++ app/default.app.php
@@ -7,7 +7,8 @@ class DefaultApp extends MallbaseApp {
         $this->assign('index', 1); // 标识当前页面是首页，用于设置导航状态
 
         $this->_config_seo(array(
-            'title' => Lang::get('mall_index') . ' - ' . Conf::get('site_title'),
+//            'title' => Lang::get('mall_index') . ' - ' . Conf::get('site_title'),
+            'title' => Conf::get('site_title'),
         ));
         $this->assign('page_description', Conf::get('site_description'));
 
