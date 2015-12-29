04f8257b0c663fb4191178b16b001b8aeb352252
farcall <linyizxw@gmail.com>
2015-12-30 00:19:38 +0800

    bug:app=member页面全部金豆并非是已消耗金豆和未消耗金豆之和

--- data/tasks.inc.php
+++ data/tasks.inc.php
@@ -5,8 +5,8 @@ return array (
   array (
     'cycle' => 'custom',
     'interval' => 3600,
-    'due_time' => 1451318911,
-    'last_time' => 1451315311,
+    'due_time' => 1451326119,
+    'last_time' => 1451322519,
   ),
 );
 
