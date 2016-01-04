5eadbbbe5e5441d24b59982f491eb5dcdff14e31
farcall <linyizxw@gmail.com>
2016-01-05 02:02:14 +0800

    屏蔽给用户添加虚拟资金的权限

--- data/tasks.inc.php
+++ data/tasks.inc.php
@@ -5,8 +5,8 @@ return array (
   array (
     'cycle' => 'custom',
     'interval' => 3600,
-    'due_time' => 1451885875,
-    'last_time' => 1451882275,
+    'due_time' => 1451932685,
+    'last_time' => 1451929085,
   ),
 );
 
