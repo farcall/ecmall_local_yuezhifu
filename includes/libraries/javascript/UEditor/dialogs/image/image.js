89034089ae1bb4e922606b5644e97ec75634ca49
farcall <linyizxw@gmail.com>
2016-01-07 15:36:10 +0800

    UEditor上传文件乱序bug http://canlynet.iteye.com/blog/2155856

--- includes/libraries/javascript/UEditor/dialogs/image/image.js
+++ includes/libraries/javascript/UEditor/dialogs/image/image.js
@@ -718,8 +718,9 @@
                     var responseText = (ret._raw || ret),
                         json = utils.str2json(responseText);
                     if (json.state == 'SUCCESS') {
-                        _this.imageList.push(json);
+                        _this.imageList[$file.index()] = json; // 按选择好的文件列表顺序存储
                         $file.append('<span class="success"></span>');
+                        $file.append('');
                     } else {
                         $file.find('.error').text(json.state).show();
                     }
--- includes/libraries/javascript/UEditor/dialogs/video/video.js
+++ includes/libraries/javascript/UEditor/dialogs/video/video.js
@@ -732,7 +732,7 @@
                     var responseText = (ret._raw || ret),
                         json = utils.str2json(responseText);
                     if (json.state == 'SUCCESS') {
-                        uploadVideoList.push({
+                        uploadVideoList[$file.index()] = ({
                             'url': json.url,
                             'type': json.type,
                             'original':json.original
