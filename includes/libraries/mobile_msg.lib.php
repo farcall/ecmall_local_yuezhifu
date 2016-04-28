872c93ae5d0c4f3abba742c394ac430a53e91554
farcall <linyizxw@gmail.com>
2016-04-28 22:42:41 +0800

    错误提醒处理：发送短信超时

--- includes/libraries/mobile_msg.lib.php
+++ includes/libraries/mobile_msg.lib.php
@@ -181,7 +181,7 @@ class Mobile_msg {
             $file_contents = file_get_contents($url);
         } else {
             $ch = curl_init();
-            $timeout = 5;
+            $timeout = 30;
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
