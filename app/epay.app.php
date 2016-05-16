e74f303a69b2038817de9e078028984f4191138c
farcall <linyizxw@gmail.com>
2016-05-17 01:27:52 +0800

    一次只能申请一次提现

--- app/epay.app.php
+++ app/epay.app.php
@@ -195,18 +195,32 @@ class EpayApp extends MemberbaseApp {
                 return;
             }
 
-            //24小时之内只能提现一次
+            //18小时之内只能提现一次
             $epaylog_data = $this->mod_epaylog->get(array(
                 'conditions' => "type=".EPAY_TX." and user_id=".$this->visitor->get('user_id'),
                 'order' => 'add_time DESC',
             ));
 
             if(!empty($epaylog_data)){
-                if((gmtime()-$epaylog_data['add_time']) < 1*12*60*60 ){
+                if((gmtime()-$epaylog_data['add_time']) < 1*18*60*60 ){
                     date_default_timezone_set('Asia/Chongqing');
-                    $this->show_warning('12小时内只能提现一次，您上次提现时间是'.date("Y-m-d H:i:s", $epaylog_data['add_time']+8*3600));
+                    $this->show_warning('18小时内只能提现一次，您上次提现时间是'.date("Y-m-d H:i:s", $epaylog_data['add_time']+8*3600));
                     return;
                 }
+            }
+
+
+            /**
+             *上一次提现申请没有处理之前不能申请
+             */
+            $check_epaylog = $this->mod_epaylog->get(array(
+                'conditions' => "type=".EPAY_TX." and complete = 0 and user_id=".$this->visitor->get('user_id'),
+                'order' => 'add_time DESC',
+            ));
+
+            if(!empty($check_epaylog)){
+                $this->show_warning('对不起，您提现时间为'.date("Y-m-d H:i:s", $check_epaylog['add_time']+8*3600).'的请求还未处理，请处理完毕之后再申请提现！');
+                return;
             }
 
             $money_row = $this->mod_epay->getrow("select * from " . DB_PREFIX . "epay where user_id='$user_id'");
